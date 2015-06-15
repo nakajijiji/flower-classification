26975a51dc32df22e0ee649b4b25884cb438dba6
nakajijiji@gmail.com <nakajikouhei@takahashi-no-MacBook-Pro.local>
2015-06-15 20:02:26 +0900

    add gunicorn

--- server.py
+++ server.py
@@ -12,6 +12,3 @@ def index():
   name = "world"
   return render_template('index.html', name = name)
 
-if __name__ == '__main__':
-  port = int(os.environ.get("PORT", 4000))
-  app.run(host='0.0.0.0', port = port, debug = True, threaded = True)
