e8440243f6db06333f864431aa20530cefffb706
中路紘平 <nakajikouhei@takahashi-no-MacBook-Pro.local>
2015-06-15 19:28:57 +0900

    flask server

---
+++ server.py
@@ -0,0 +1,16 @@
+import os
+import json
+import datetime
+from flask import Flask, jsonify, render_template, request, g, redirect, url_for
+from flask.ext.cors import cross_origin
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+@cross_origin(headers='Content-Type')
+def index():
+  name = "world"
+  return render_template('index.html', name = name)
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port = 4000, debug = True, threaded = True)
